bc5e7e83a0e5510ecef7193132872f56b2814eaa
Marcel Maas <maas.marcel@gmail.com>
2017-08-01 16:58:24 +0200

    Created initial task list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {TaskModule} from './task/task.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TaskModule
   ],
   providers: [],
   bootstrap: [AppComponent]
